120f78ed5062fe1638ef6e666a7490f31254c2ed
johnnyjuarez <johnnyjuarez1992@gmail.com>
2019-05-14 01:46:11 -0500

    added function to toggle class based on window size for font changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 $(".navbar-nav>li>a").on("click", function() {
   $(".navbar-collapse").collapse("hide");
 });
+// fade navbar when scrolled
 $(function() {
   $(document).scroll(function() {
     var $nav = $("#mainNavbar");
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
+// function to change add or remove "small" class depending on window size
+$(function() {
+  var $window = $(window),
+    $toggle = $(".project-toggle");
+
+  $window.resize(function resize() {
+    if ($window.width() > 460) {
+      return $toggle.removeClass("small");
+    } else {
+      return $toggle.addClass("small");
+    }
   });
 });
