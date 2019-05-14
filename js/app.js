9f31838a70188c7d39645d3f953e496654f7cb83
johnnyjuarez <johnnyjuarez1992@gmail.com>
2019-05-14 04:05:53 -0500

    added contact.html and stylings, as well as added an about section in the index.html

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(function() {
   });
 });
 
-// function to change add or remove "small" class depending on window size
+// function to change add or remove "small" class depending on window size on project page
 $(function() {
   var $window = $(window),
     $toggle = $(".project-toggle");
@@ -19,6 +19,19 @@ $(function() {
       return $toggle.removeClass("small");
     } else {
       return $toggle.addClass("small");
+    }
+  });
+});
+// function to remove fa-9x
+$(function() {
+  var $window = $(window),
+    $toggle = $(".contact-toggle");
+
+  $window.resize(function resize() {
+    if ($window.width() > 460) {
+      return $toggle.removeClass("fa-9x");
+    } else {
+      return $toggle.addClass("fa-9x");
     }
   });
 });
